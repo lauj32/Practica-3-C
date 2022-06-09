8eac928b4eca35928d62ccc3cd26ecb6746849e4
lauj32 <105667937+lauj32@users.noreply.github.com>
2022-06-09 12:33:28 -0300

    Create create_word.h

---
+++ create_word.h
@@ -0,0 +1,12 @@
+#ifndef _CREATE_WORD_H
+#define _CREATE_WORD_H
+void delete_word(char *);
+
+short search_letter(const char * , char );
+
+char * delete_letter(char * , char);
+
+char * add_letter(char * , char);
+
+
+#endif
